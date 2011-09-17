232736a2f25b97fe94e177d320c379257ef7a92c
Ludovic Galabru <ludovic@me.com>
2011-09-17 14:50:01 +0200

    Bug fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,13 +12,16 @@ Hackathon::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
+
+  config.action_dispatch.x_sendfile_header = nil  # as per Heroku support 
+  config.force_ssl = true 
 
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
